e4db87755508a44db1abc5a9d28f66339478a9d2
gengmingzhu <915358123@qq.com>
2014-10-13 11:21:27 +0800

    oooooo

--- src/tt/Heloopo.java
+++ src/tt/Heloopo.java
@@ -3,8 +3,11 @@ package tt;
 public class Heloopo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("--hello--");
+		System.out.println("--hello--");
+		System.out.println("--hello--");
+		System.out.println("--hello--");
+		System.out.println("--hello--");	System.out.println("--hello--");
 	}
 
 }
